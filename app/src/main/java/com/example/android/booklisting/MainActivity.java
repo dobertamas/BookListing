ccb4385052315093225482d02576d56873bcfb09
Tamas Dober <tamas.dober@mobileforming.com>
2017-07-24 23:21:19 -0700

    Adding skeleton of BookAsyncTask

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.android.booklisting;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.search_books_button) Button mSearchBooksButton;
+    @InjectView(R.id.enter_search_term) EditText mSearchTermEditText;
+
+    String mSearchTerm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        mSearchBooksButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                /*Intent productListIntent = new Intent(MainActivity.this, ProductDetailActivity.class);
+                startActivity(productListIntent);*/
+
+                mSearchTerm = mSearchTermEditText.getText().toString().trim();
+                if (mSearchTerm == null || mSearchTerm.isEmpty()) {
+                    return;
+                }
+                // Kick off an {@link AsyncTask} to perform the network request
+                BookAsyncTask task = new BookAsyncTask();
+                task.execute();
+            }
+        });
+    }
+
+    // https://www.google.com/#q=soccer
+    private class BookAsyncTask extends AsyncTask<URL, Void, ArrayList<Book>> {
+
+        @Override protected ArrayList<Book> doInBackground(URL... urls) {
+            ArrayList<Book> bookList = new ArrayList<>();
+            return bookList;
+        }
     }
 }
