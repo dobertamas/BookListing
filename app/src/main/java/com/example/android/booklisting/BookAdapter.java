fbe95e7a476b7af9880154ccb86b523191d1d8a8
Tamas Dober <tamas.dober@mobileforming.com>
2017-07-25 22:41:09 -0700

    Adding ViewHolder to the Adapter

--- app/src/main/java/com/example/android/booklisting/BookAdapter.java
+++ app/src/main/java/com/example/android/booklisting/BookAdapter.java
@@ -3,7 +3,6 @@ package com.example.android.booklisting;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,25 +20,37 @@ class BookAdapter extends ArrayAdapter<Book> {
     @NonNull @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
-        Book book = getItem(position);
+        ViewHolder viewHolder;
 
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
-            Log.d(LOG_TAG, " creating new view ");
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
         }
-        // Lookup view for data population
-        TextView titleTextView = convertView.findViewById(R.id.list_item_title);
-        TextView authorTextView = convertView.findViewById(R.id.list_item_author);
+        else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
 
-        // Populate the data into the template view using the data object
+        Book book = getItem(position);
+
+        // Populate the data into the view using the data object
         assert book != null;
-        titleTextView.setText(book.getTitle());
-        authorTextView.setText(book.getAuthor());
+        viewHolder.titleTextView.setText(book.getTitle());
+        viewHolder.authorTextView.setText(book.getAuthor());
 
         // Return the completed view to render on screen
         return convertView;
     }
 
+    private class ViewHolder {
+        TextView titleTextView;
+        TextView authorTextView;
+
+        ViewHolder(View view) {
+            this.titleTextView = view.findViewById(R.id.list_item_title);
+            this.authorTextView = view.findViewById(R.id.list_item_author);
+        }
+    }
 }
